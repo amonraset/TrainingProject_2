c5c89e22073d34c5c01b2f2b2c248b380bc9ba2a
amonraset <minia777@icloud.com>
2024-10-27 00:49:43 +0300

    Initial Commit

---
+++ TrainingProject_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainingProject_2
+//
+//  Created by sm on 27.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
