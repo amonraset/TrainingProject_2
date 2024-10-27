bd901c3ab735eb306e5b5ab21bf963108843cec3
amonraset <minia777@icloud.com>
2024-10-27 17:19:41 +0300

    step

--- TrainingProject_2/ViewController.swift
+++ TrainingProject_2/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        tableView.sectionHeaderHeight = 32
+    }
+}
+
+let words = [
+    ["Apple", "Pear", "Watermelon", "Carrot", "Banana"],
+    ["Carrot", "Pickle", "Potato", "Tomato"]
+    ]
+
+let header = ["Fruits", "Vegans"]
+
+ 
+extension ViewController: UITableViewDataSource {
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return words.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let vegFrutArray = words[section]
+        return vegFrutArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return header[section]
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell
+        
+        if let reusedCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            cell = reusedCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        var content = cell.defaultContentConfiguration()
+        content.text = words[indexPath.section][indexPath.row]
+        cell.contentConfiguration = content
+        return cell
     }
 
 
 }
-
